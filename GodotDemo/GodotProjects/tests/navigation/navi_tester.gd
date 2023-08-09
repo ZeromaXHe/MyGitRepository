dc393337cd48b8821cab034ca436bef67014df21
Zhu XH <zhuxiaohe95@163.com>
2023-08-09 18:01:44 +0800

    test(GodotDemo): GodotProjects 导航功能测试

---
+++ GodotDemo/GodotProjects/tests/navigation/navi_tester.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var movement_speed: float = 200.0
+# Target 的坐标
+var movement_target_position: Vector2 = Vector2(563.0, 310.0)
+
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(movement_target_position)
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	var new_velocity: Vector2 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * movement_speed
+
+	velocity = new_velocity
+	move_and_slide()
