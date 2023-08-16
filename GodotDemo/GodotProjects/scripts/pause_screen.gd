6e789a7130d9d4d1726a075d0f738a8a10a25b76
Zhu XH <zhuxiaohe95@163.com>
2023-08-16 20:05:06 +0800

    fix(GodotDemo): GodotProjects 修复 bug - 暂停页面跳转主菜单场景错误

--- GodotDemo/GodotProjects/scripts/pause_screen.gd
+++ GodotDemo/GodotProjects/scripts/pause_screen.gd
@@ -14,7 +14,7 @@ func _on_continue_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
 
 
 func _on_quit_button_pressed() -> void:
