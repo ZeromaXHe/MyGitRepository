5473151cf95d9dd2b4a6456deee49ebb6eefd3db
Zhu XH <zhuxiaohe95@163.com>
2023-08-25 15:31:52 +0800

    refactor(GodotDemo): GodotProjects 俯视角射击游戏 - 得分板数据行改为自动根据人数生成

--- GodotDemo/GodotProjects/scripts/score_board.gd
+++ GodotDemo/GodotProjects/scripts/score_board.gd
@@ -26,41 +26,27 @@ class ScoreInfo:
 
 
 class BoardLabelRow:
-	var name: RichTextLabel
-	var kill: Label
-	var dead: Label
-	var capture: Label
-	var score: Label
+	var name_label: RichTextLabel
+	var kill_label: Label
+	var dead_label: Label
+	var capture_label: Label
+	var score_label: Label
 
 
 func _ready() -> void:
-	# 初始化 player_rows 和 enemy_rows
-	for i in range(4):
-		init_row(player_stat, player_rows, i)
-		init_row(enemy_stat, enemy_rows, i)
-	
 	GlobalSignals.actor_killed.connect(handle_actor_killed)
 	
 	for base in GlobalMediator.capturable_base_manager.capturable_bases:
 		(base as CapturableBase).base_captured.connect(handle_base_captured)
 
 
-func init_row(stat: GridContainer, rows: Array, idx: int):
-	var row = BoardLabelRow.new()
-	row.name = stat.get_node("Name"+ str(idx + 1)) as RichTextLabel
-	row.kill = stat.get_node("Kill" + str(idx + 1)) as Label
-	row.dead = stat.get_node("Dead" + str(idx + 1)) as Label
-	row.capture = stat.get_node("Capture" + str(idx + 1)) as Label
-	row.score = stat.get_node("Score" + str(idx + 1)) as Label
-	rows.append(row)
-
-
 func update_score_board():
 	player_team_score_infos.sort_custom(sort_score_info)
 	enemy_team_score_infos.sort_custom(sort_score_info)
 	
-	for i in range(4):
+	for i in range(player_rows.size()):
 		update_row(player_rows[i], player_team_score_infos[i], true)
+	for i in range(enemy_rows.size()):
 		update_row(enemy_rows[i], enemy_team_score_infos[i], false)
 
 
@@ -72,11 +58,11 @@ func sort_score_info(a: ScoreInfo, b: ScoreInfo):
 
 
 func update_row(row: BoardLabelRow, score_info: ScoreInfo, player_team: bool):
-	row.name.text = name_bbcode_wrapper(score_info.name, player_team)
-	row.kill.text = str(score_info.kill)
-	row.dead.text = str(score_info.dead)
-	row.capture.text = str(score_info.capture)
-	row.score.text = str(score_info.score)
+	row.name_label.text = name_bbcode_wrapper(score_info.name, player_team)
+	row.kill_label.text = str(score_info.kill)
+	row.dead_label.text = str(score_info.dead)
+	row.capture_label.text = str(score_info.capture)
+	row.score_label.text = str(score_info.score)
 
 
 func name_bbcode_wrapper(name: String, player_team: bool) -> String:
@@ -91,6 +77,7 @@ func recalc_score(score_info: ScoreInfo):
 
 
 func handle_actor_inited(actor: Actor):
+	# 初始化得分信息数据结构
 	var score_info := ScoreInfo.new()
 	score_info.name = actor.name
 	score_info.timestamp = Time.get_unix_time_from_system()
@@ -99,10 +86,59 @@ func handle_actor_inited(actor: Actor):
 		player_team_score_infos.append(score_info)
 	else:
 		enemy_team_score_infos.append(score_info)
+	print("init ", actor.name, "'s score board row...")
+	# 初始化得分板界面上的一行
+	init_board_label_row(actor)
+
+
+func init_board_label_row(actor: Actor):
+	var is_player_team: bool = (actor.team.side == Team.Side.PLAYER)
+	# 好像并不需要等待 ready 信号，stat 不会为空
+	var stat: GridContainer = player_stat if is_player_team else enemy_stat
+	var rows: Array = player_rows if is_player_team else enemy_rows
 	
-	# TODO: 现在代码和场景里都是写死的数量，后续优化
-	if name_to_score_info_dict.size() == 8:
-		update_score_board()
+	var row = BoardLabelRow.new()
+	# 名字标签
+	row.name_label = RichTextLabel.new()
+	row.name_label.name = "Name"
+	row.name_label.bbcode_enabled = true
+	row.name_label.text = name_bbcode_wrapper(actor.name, is_player_team)
+	row.name_label.fit_content = true
+	stat.add_child(row.name_label)
+	stat.add_child(VSeparator.new())
+	# 击杀数标签
+	row.kill_label = Label.new()
+	row.kill_label.name = "Kill"
+	row.kill_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	row.kill_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	row.kill_label.text = "0"
+	stat.add_child(row.kill_label)
+	stat.add_child(VSeparator.new())
+	# 死亡数标签
+	row.dead_label = Label.new()
+	row.dead_label.name = "Dead"
+	row.dead_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	row.dead_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	row.dead_label.text = "0"
+	stat.add_child(row.dead_label)
+	stat.add_child(VSeparator.new())
+	# 占领数标签
+	row.capture_label = Label.new()
+	row.capture_label.name = "Capture"
+	row.capture_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	row.capture_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	row.capture_label.text = "0"
+	stat.add_child(row.capture_label)
+	stat.add_child(VSeparator.new())
+	# 得分标签
+	row.score_label = Label.new()
+	row.score_label.name = "Score"
+	row.score_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	row.score_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	row.score_label.text = "0"
+	stat.add_child(row.score_label)
+	
+	rows.append(row)
 
 
 func handle_actor_killed(killed: Actor, killer: Actor, _weapon: Weapon):
