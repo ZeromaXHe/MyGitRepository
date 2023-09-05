d367ef709599b561bac4a2bfb5e33efb37273459
Zhu XH <zhuxiaohe95@163.com>
2023-09-05 11:53:15 +0800

    feature(GodotDemo): Civ6Copy 仿文明战棋游戏 - 鼠标滚轮控制镜头缩放

--- GodotDemo/Civ6Copy/scenes/camera.gd
+++ GodotDemo/Civ6Copy/scenes/camera.gd
@@ -1,25 +1,45 @@
 extends Camera2D
 
 
-var _previous_position: Vector2 = Vector2(0, 0)
+@export var MIN_ZOOM := Vector2(0.5, 0.5)
+@export var MAX_ZOOM := Vector2(2, 2)
+
+var _previous_position := Vector2(0, 0)
 var _move_camera: bool = false
+var _final_zoom: Vector2 = zoom
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	# 拖拽镜头的实现
-	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
-		get_viewport().set_input_as_handled()
-		if event.is_pressed():
-			_previous_position = event.position
-			_move_camera = true
-		else:
-			_move_camera = false
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			# 开始拖拽镜头
+			get_viewport().set_input_as_handled()
+			if event.is_pressed():
+				_previous_position = event.position
+				_move_camera = true
+			else:
+				_move_camera = false
+		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			# 镜头放大
+			get_viewport().set_input_as_handled()
+			_final_zoom = clamp(_final_zoom + Vector2(0.05, 0.05), MIN_ZOOM, MAX_ZOOM)
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "zoom", _final_zoom, 0.1)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			# 镜头缩小
+			get_viewport().set_input_as_handled()
+			_final_zoom = clamp(_final_zoom - Vector2(0.05, 0.05), MIN_ZOOM, MAX_ZOOM)
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "zoom", _final_zoom, 0.1)
 	elif event is InputEventMouseMotion && _move_camera:
+		# 拖拽镜头过程中
 		get_viewport().set_input_as_handled()
 		position += (_previous_position - event.position)
 		# 限制镜头移动范围
 		var diff_x = zoom.x * get_viewport().size.x / 2
 		var diff_y = zoom.y * get_viewport().size.y / 2
+		# 直接对 Vector2 而不分别对 x、y 进行 clamp() 的话，貌似会 bug。
+		# 因为 Vector2 的大小判断逻辑是 x 处于范围内的话，就不会处理 y 了。
 		position.x = clamp(position.x, -1500 + diff_x, 1500 - diff_x)
 		position.y = clamp(position.y, -500 + diff_y, 500 - diff_y)
 		_previous_position = event.position
