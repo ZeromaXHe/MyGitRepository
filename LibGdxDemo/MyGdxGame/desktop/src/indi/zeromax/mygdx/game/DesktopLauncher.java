94250f7762da75e65395de8269b43b80beed2ee8
Zhu XH <zhuxiaohe95@163.com>
2024-06-17 01:36:59 +0800

    feature(MyGdxGame): Scala + LibGDX 项目基础结构

---
+++ LibGdxDemo/MyGdxGame/desktop/src/indi/zeromax/mygdx/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package indi.zeromax.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import indi.zeromax.mygdx.game.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("MyGdxGame");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
