d6da7a5711400ec19acf22666f7ab874d8372226
Zhu XH <zhuxiaohe95@163.com>
2025-01-04 19:21:42 +0800

    feature(StayAtHomeDevFPS): 参考 StayAtHomeDev FPS 教程 - E22 武器空闲动画

--- GodotDemo/StayAtHomeDevFPS/meshes/weapons/init_weapon.gd
+++ GodotDemo/StayAtHomeDevFPS/meshes/weapons/init_weapon.gd
@@ -9,11 +9,24 @@ extends Node3D
 		if is_node_ready() and Engine.is_editor_hint():
 			print("更换武器！")
 			load_weapon()
+@export var sway_noise: NoiseTexture2D
+@export var sway_speed: float = 1.2
+@export var reset: bool = false:
+	set(value):
+		reset = value
+		if is_node_ready() and Engine.is_editor_hint():
+			load_weapon()
 
 @onready var weapon_mesh: MeshInstance3D = %WeaponMesh
 @onready var weapon_shadow: MeshInstance3D = %WeaponShadow
 
 var mouse_movement: Vector2
+var random_sway_x
+var random_sway_y
+var random_sway_amount: float
+var time: float = 0.0
+var idle_sway_adjustment
+var idle_sway_rotation_strength
 
 func _ready() -> void:
 	if owner != null && owner != self: #好像 GDScript 就不会阻塞自己……
@@ -39,22 +52,43 @@ func load_weapon() -> void:
 	rotation_degrees = WEAPON_TYPE.rotation
 	weapon_shadow.mesh = WEAPON_TYPE.mesh # 自己加的。为啥教程里不设置 mesh？
 	weapon_shadow.visible = WEAPON_TYPE.shadow
+	
+	idle_sway_adjustment = WEAPON_TYPE.idle_sway_adjustment
+	idle_sway_rotation_strength = WEAPON_TYPE.idle_sway_rotation_strength
+	random_sway_amount = WEAPON_TYPE.random_sway_amount
 
 
 func sway_weapon(delta) -> void:
+	var sway_random: float = get_sway_noise()
+	var sway_random_adjusted: float = sway_random * idle_sway_adjustment
+
+	time += delta * (sway_speed + sway_random)
+	random_sway_x = sin(time * 1.5 + sway_random_adjusted) / random_sway_amount
+	random_sway_y = sin(time - sway_random_adjusted) / random_sway_amount
+
 	mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min, WEAPON_TYPE.sway_max)
-	position.x = lerp(position.x, \
-		WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position) * delta, \
+	position.x = lerp(position.x, WEAPON_TYPE.position.x - \
+		(mouse_movement.x * WEAPON_TYPE.sway_amount_position + random_sway_x) * delta, \
 		WEAPON_TYPE.sway_speed_position)
-	position.y = lerp(position.y, \
-		WEAPON_TYPE.position.y + (mouse_movement.y * WEAPON_TYPE.sway_amount_position) * delta, \
+	position.y = lerp(position.y, WEAPON_TYPE.position.y + \
+		(mouse_movement.y * WEAPON_TYPE.sway_amount_position + random_sway_y) * delta, \
 		WEAPON_TYPE.sway_speed_position)
-	rotation_degrees.y = lerp(rotation_degrees.y, \
-		WEAPON_TYPE.rotation.y + (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation) * delta, \
+	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y + \
+		(mouse_movement.x * WEAPON_TYPE.sway_amount_rotation + \
+		random_sway_y * idle_sway_rotation_strength) * delta, \
 		WEAPON_TYPE.sway_speed_rotation)
-	rotation_degrees.x = lerp(rotation_degrees.x, \
-		WEAPON_TYPE.rotation.x - (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation) * delta, \
+	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x - \
+		(mouse_movement.y * WEAPON_TYPE.sway_amount_rotation + \
+		random_sway_x * idle_sway_rotation_strength) * delta, \
 		WEAPON_TYPE.sway_speed_rotation)
+
+
+func get_sway_noise() -> float:
+	var player_position := Vector3.ZERO
+	if not Engine.is_editor_hint():
+		player_position = Global.player.global_position
+	var noise_location: float = sway_noise.noise.get_noise_2d(player_position.x, player_position.y)
+	return noise_location
 
 
 func _physics_process(delta: float) -> void:
--- GodotDemo/StayAtHomeDevFPS/meshes/weapons/weapons.gd
+++ GodotDemo/StayAtHomeDevFPS/meshes/weapons/weapons.gd
@@ -5,6 +5,7 @@ extends Resource
 @export_category("武器朝向")
 @export var position: Vector3
 @export var rotation: Vector3
+@export var scale: Vector3
 @export_category("武器摇摆")
 @export var sway_min := Vector2(-20.0, -20.0)
 @export var sway_max := Vector2(20.0, 20.0)
@@ -12,6 +13,9 @@ extends Resource
 @export_range(0, 0.2, 0.01) var sway_speed_rotation: float = 0.1
 @export_range(0, 0.25, 0.01) var sway_amount_position: float = 0.1
 @export_range(0, 50, 0.1) var sway_amount_rotation: float = 30.0
+@export var idle_sway_adjustment: float = 10.0
+@export var idle_sway_rotation_strength: float = 300.0
+@export_range(0.1, 10.0, 0.1) var random_sway_amount: float = 5.0
 @export_category("视觉设置")
 @export var mesh: Mesh
 @export var shadow: bool
