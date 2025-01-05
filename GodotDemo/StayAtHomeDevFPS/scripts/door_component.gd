acb62514189e5ca3edd5c49e3048da25aa5e90bd
Zhu XH <zhuxiaohe95@163.com>
2025-01-05 13:10:24 +0800

    feature(StayAtHomeDevFPS): 参考 StayAtHomeDev FPS 教程 - E35 创建旋转门 ~ E37 如何打开和关闭门

--- GodotDemo/StayAtHomeDevFPS/scripts/door_component.gd
+++ GodotDemo/StayAtHomeDevFPS/scripts/door_component.gd
@@ -1,41 +1,101 @@
 class_name DoorComponent
 extends Node
 
-@export var direction: Vector3
+enum DoorType {SLIDING, ROTATING}
+enum ForwardDirection {X, Y, Z}
+enum DoorStatus {OPEN, CLOSED}
+
+@export_group("门设置")
+@export var door_type: DoorType
+@export var forward_direction: ForwardDirection
+@export var movement_direction: Vector3
+@export var rotation := Vector3.UP
+@export var rotation_amount : float = 90.0
 @export var door_size: Vector3
-@export var speed: float = 0.5
+@export_group("关门设置")
+@export var close_automatically: bool = true
 @export var close_time: float = 2.0
+@export_group("Tween 设置")
+@export var speed: float = 0.5
 @export var transition: Tween.TransitionType
 @export var easing: Tween.EaseType
 
 var parent
 var orig_pos: Vector3
+var orig_rot: Vector3
+var rotation_adjustment: float
+var door_direction: Vector3
+var door_status := DoorStatus.CLOSED
 
 
 func _ready() -> void:
 	parent = get_parent()
 	orig_pos = parent.position
+	orig_rot = parent.rotation
 	parent.ready.connect(connect_parent)
 
 
 func connect_parent() -> void:
-	print("door interacted signal connecting: ", parent.has_user_signal("interacted"))
-	parent.connect("interacted", open_door)
+	#print("door interacted signal connecting: ", parent.has_user_signal("interacted"))
+	parent.connect("interacted", check_door)
+
+
+func check_door() -> void:
+	match forward_direction:
+		ForwardDirection.X:
+			door_direction = parent.global_transform.basis.x
+		ForwardDirection.Y:
+			door_direction = parent.global_transform.basis.y
+		ForwardDirection.Z:
+			door_direction = parent.global_transform.basis.z
+	var door_position: Vector3 = parent.global_position
+	var player_position: Vector3 = Global.player.global_position
+	var direction_to_player: Vector3 = door_position.direction_to(player_position)
+	var door_dot: float = direction_to_player.dot(door_direction)
+	if door_dot < 0:
+		rotation_adjustment = 1
+	else:
+		rotation_adjustment = -1
+	#print("check_door forward:", forward_direction, ", door_direction: ", door_direction, \
+		#", door_position: ", door_position, ", player_position: ", player_position, \
+		#", door_dot: ", door_dot)
+	match door_status:
+		DoorStatus.CLOSED:
+			open_door()
+		DoorStatus.OPEN:
+			close_door()
 
 
 func open_door() -> void:
-	print("open door")
+	#print("open door")
+	door_status = DoorStatus.OPEN
 	var tween = get_tree().create_tween()
-	tween.tween_property(parent, "position", orig_pos + (direction * door_size), speed) \
-		.set_trans(transition) \
-		.set_ease(easing)
-	tween.tween_interval(close_time)
-	tween.tween_callback(close_door)
+	match door_type:
+		DoorType.SLIDING:
+			tween.tween_property(parent, "position", \
+				orig_pos + (movement_direction * door_size), speed) \
+				.set_trans(transition) \
+				.set_ease(easing)
+		DoorType.ROTATING:
+			tween.tween_property(parent, "rotation", \
+				orig_rot + (rotation * rotation_adjustment * deg_to_rad(rotation_amount)), speed) \
+				.set_trans(transition) \
+				.set_ease(easing)
+	if close_automatically:
+		tween.tween_interval(close_time)
+		tween.tween_callback(close_door)
 
 
 func close_door() -> void:
-	print("close door")
+	#print("close door")
+	door_status = DoorStatus.CLOSED
 	var tween = get_tree().create_tween()
-	tween.tween_property(parent, "position", orig_pos, speed) \
-		.set_trans(transition) \
-		.set_ease(easing)
+	match door_type:
+		DoorType.SLIDING:
+			tween.tween_property(parent, "position", orig_pos, speed) \
+				.set_trans(transition) \
+				.set_ease(easing)
+		DoorType.ROTATING:
+			tween.tween_property(parent, "rotation", orig_rot, speed) \
+				.set_trans(transition) \
+				.set_ease(easing)
