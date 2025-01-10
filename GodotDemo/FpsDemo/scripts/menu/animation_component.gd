20192fd1a89148e1c1c226aa71d0a2056982d105
Zhu XH <zhuxiaohe95@163.com>
2025-01-10 23:12:53 +0800

    feature(FpsDemo): 参考 StayAtHomeDev UI 教程 - E05 如何为 Godot UI 元素的进入设置动画

--- GodotDemo/FpsDemo/scripts/menu/animation_component.gd
+++ GodotDemo/FpsDemo/scripts/menu/animation_component.gd
@@ -1,8 +1,11 @@
 class_name AnimationComponent
 extends Node
 
+signal entered
+
 @export_group("选项")
 @export var from_center: bool = true
+@export var enter_animation: bool = false
 @export var parallel_animations: bool = true
 @export var properties: Array = [
 	"scale",
@@ -23,10 +26,25 @@ extends Node
 @export var hover_size: Vector2
 @export var hover_modulate := Color.WHITE
 
+@export_group("进入设置")
+@export var wait_for: AnimationComponent
+@export var enter_time: float = 0.1
+@export var enter_delay: float = 0.0
+@export var enter_transition: Tween.TransitionType
+@export var enter_easing: Tween.EaseType
+@export var enter_position: Vector2
+@export var enter_scale := Vector2.ONE
+@export var enter_rotation: float
+@export var enter_size: Vector2
+@export var enter_modulate := Color.WHITE
+
 var target: Control
 var default_scale: Vector2
 var hover_values: Dictionary
 var default_values: Dictionary
+var enter_values: Dictionary
+
+const IMMEDIATE_TRANSITION = Tween.TRANS_LINEAR
 
 
 # Called when the node enters the scene tree for the first time.
@@ -36,10 +54,13 @@ func _ready() -> void:
 
 
 func connect_signals() -> void:
-	target.mouse_entered.connect(add_tween.bind(
-		hover_values, parallel_animations, hover_time, hover_delay, hover_transition, hover_easing))
-	target.mouse_exited.connect(add_tween.bind(
-		default_values, parallel_animations, hover_time, hover_delay, hover_transition, hover_easing))
+	target.mouse_entered.connect(add_tween.bind(hover_values, parallel_animations,
+		hover_time, hover_delay, hover_transition, hover_easing, false))
+	target.mouse_exited.connect(add_tween.bind(default_values, parallel_animations,
+		hover_time, hover_delay, hover_transition, hover_easing, false))
+	if wait_for:
+		wait_for.entered.connect(add_tween.bind(default_values, parallel_animations,
+			enter_time, enter_delay, enter_transition, enter_easing, true))
 
 
 func setup() -> void:
@@ -60,18 +81,40 @@ func setup() -> void:
 		"size": target.size + hover_size,
 		"self_modulate": hover_modulate,
 	}
+	enter_values = {
+		"scale": enter_scale,
+		"position": target.position + enter_position,
+		"rotation": target.rotation + deg_to_rad(enter_rotation),
+		"size": target.size + enter_size,
+		"self_modulate": enter_modulate,
+	}
 	connect_signals()
+	if enter_animation:
+		on_enter()
+	else:
+		entered.emit()
+
+
+func on_enter() -> void:
+	add_tween(enter_values, true, 0.0, 0.0, IMMEDIATE_TRANSITION, hover_easing, false)
+	if wait_for:
+		return
+	add_tween(default_values, parallel_animations, enter_time, enter_delay,
+		enter_transition, enter_easing, true)
 
 
 func add_tween(values: Dictionary, parallel: bool, seconds: float, delay: float,
-		transition: Tween.TransitionType, easing: Tween.EaseType) -> void:
+		transition: Tween.TransitionType, easing: Tween.EaseType, entering: bool) -> void:
 	var tween: Tween = get_tree().create_tween()
 	tween.set_parallel(parallel)
-	tween.tween_interval(delay) # 明明就是一行的事情，被 StayAtHomeDev 又折腾一集……
-	#tween.pause()
+	#tween.tween_interval(delay) # 发现这个在 E05 就不行了…… 捂脸
+	tween.pause()
 	for property: String in properties:
 		tween.tween_property(target, property, values[property], seconds) \
 			.set_trans(transition) \
 			.set_ease(easing)
-	#await get_tree().create_timer(delay).timeout
-	#tween.play()
+	await get_tree().create_timer(delay).timeout
+	tween.play()
+	if entering:
+		await tween.finished
+		entered.emit()
