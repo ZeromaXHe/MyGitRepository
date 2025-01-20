a730c72fa8307cd5ff841ad8bb47bcf95f49f775
Zhu XH <zhuxiaohe95@163.com>
2025-01-20 15:38:55 +0800

    feature(CatlikeCoding): 参考 Basics - 7 Organic Variety

--- GodotDemo/CatlikeCoding/CatlikeCodingFS/Basics1/FractalFS.fs
+++ GodotDemo/CatlikeCoding/CatlikeCodingFS/Basics1/FractalFS.fs
@@ -4,27 +4,25 @@ open Godot
 
 type FractalPart =
     struct
-        val mutable direction: Vector3
         val mutable worldPosition: Vector3
         val mutable rotation: Quaternion
         val mutable worldRotation: Quaternion
         val mutable spinAngle: float32
+        val mutable maxSagAngle: float32
+        val mutable spinVelocity: float32
 
-        new(direction, rotation) =
-            { direction = direction
-              worldPosition = Vector3.Zero
+        new(rotation, maxSagAngle, spinVelocity) =
+            { worldPosition = Vector3.Zero
               rotation = rotation
               worldRotation = Quaternion.Identity
-              spinAngle = 0f }
+              spinAngle = 0f
+              maxSagAngle = maxSagAngle
+              spinVelocity = spinVelocity }
     end
 
 [<AbstractClass>]
 type FractalFS() =
     inherit Node3D()
-
-    let directions =
-        // Godot 和 Unity 前后相反
-        [| Vector3.Up; Vector3.Right; Vector3.Left; Vector3.Back; Vector3.Forward |]
 
     // 这里的 rotations 是四元数！一开始以为是 Vector3…… 然后用 Rotation 修改旋转就各种 bug 调了一下午
     let rotations =
@@ -36,46 +34,107 @@ type FractalFS() =
 
     let mutable parts: FractalPart array array = null
     let mutable matrices: Transform3D array array = null
-    let mutable matricesBuffers: float32 array = null
+    let mutable matricesBuffer: float32 array = null
+    let mutable matricesBufferLeaves: float32 array = null
+    let stride = 16 // 12 Transform + 4 Color
+    let mutable sequenceNumbers: Vector4 array = null
 
     let mutable ready = false
     let mutable multiMeshIns: MultiMeshInstance3D = null
+    let mutable multiMeshInsLeaves: MultiMeshInstance3D = null
 
     abstract Depth: int with get, set
     abstract MeshF: Mesh with get, set
+    abstract LeafMesh: Mesh with get, set
     abstract MaterialF: Material with get, set
+    abstract GradientA: Gradient with get, set
+    abstract GradientB: Gradient with get, set
+    abstract LeafColorA: Color with get, set
+    abstract LeafColorB: Color with get, set
+    abstract MaxSagAngleA: float32 with get, set
+    abstract MaxSagAngleB: float32 with get, set
+    abstract SpinSpeedA: float32 with get, set
+    abstract SpinSpeedB: float32 with get, set
+    abstract ReverseSpinChance: float32 with get, set
 
     member this.CreatePart childIndex =
-        FractalPart(directions[childIndex], rotations[childIndex])
+        FractalPart(
+            rotations[childIndex],
+            Mathf.DegToRad(float32 (GD.RandRange(float this.MaxSagAngleA, float this.MaxSagAngleB))),
+            (if GD.Randf() < this.ReverseSpinChance then -1f else 1f)
+            * Mathf.DegToRad(float32 (GD.RandRange(float this.SpinSpeedA, float this.SpinSpeedB)))
+        )
 
-    member this.Transform3dToMatricesBuffers idx (transform: Transform3D) =
-        matricesBuffers[idx * 12] <- transform[0, 0]
-        matricesBuffers[idx * 12 + 1] <- transform[1, 0]
-        matricesBuffers[idx * 12 + 2] <- transform[2, 0]
-        matricesBuffers[idx * 12 + 3] <- transform[3, 0]
-        matricesBuffers[idx * 12 + 4] <- transform[0, 1]
-        matricesBuffers[idx * 12 + 5] <- transform[1, 1]
-        matricesBuffers[idx * 12 + 6] <- transform[2, 1]
-        matricesBuffers[idx * 12 + 7] <- transform[3, 1]
-        matricesBuffers[idx * 12 + 8] <- transform[0, 2]
-        matricesBuffers[idx * 12 + 9] <- transform[1, 2]
-        matricesBuffers[idx * 12 + 10] <- transform[2, 2]
-        matricesBuffers[idx * 12 + 11] <- transform[3, 2]
+    member this.Transform3dToMatricesBuffer index li (transform: Transform3D) =
+        let leafIndex = this.Depth - 1
+
+        let buffer =
+            if li = leafIndex then
+                matricesBufferLeaves
+            else
+                matricesBuffer
+
+        let idx =
+            if li = leafIndex then
+                index - matricesBuffer.Length / stride
+            else
+                index
+        // 变换信息
+        buffer[idx * stride] <- transform[0, 0]
+        buffer[idx * stride + 1] <- transform[1, 0]
+        buffer[idx * stride + 2] <- transform[2, 0]
+        buffer[idx * stride + 3] <- transform[3, 0]
+        buffer[idx * stride + 4] <- transform[0, 1]
+        buffer[idx * stride + 5] <- transform[1, 1]
+        buffer[idx * stride + 6] <- transform[2, 1]
+        buffer[idx * stride + 7] <- transform[3, 1]
+        buffer[idx * stride + 8] <- transform[0, 2]
+        buffer[idx * stride + 9] <- transform[1, 2]
+        buffer[idx * stride + 10] <- transform[2, 2]
+        buffer[idx * stride + 11] <- transform[3, 2]
+        // 颜色信息
+        let color =
+            if this.GradientA = null || this.GradientB = null then
+                let offset = float32 li / float32 leafIndex
+                Colors.Yellow.Lerp(Colors.Red, offset)
+            else
+                let colorA, colorB =
+                    if li = leafIndex then
+                        this.LeafColorA, this.LeafColorB
+                    else
+                        let gradientInterpolator = float32 li / float32 (this.Depth - 2)
+                        this.GradientA.Sample(gradientInterpolator), this.GradientB.Sample(gradientInterpolator)
+
+                let rand = (float32 idx * sequenceNumbers[li].X + sequenceNumbers[li].Y) % 1f
+                colorA.Lerp(colorB, rand)
+
+        buffer[idx * stride + 12] <- color.R
+        buffer[idx * stride + 13] <- color.G
+        buffer[idx * stride + 14] <- color.B
+        buffer[idx * stride + 15] <- color.A
 
     override this._Ready() =
         parts <- Array.zeroCreate this.Depth
         matrices <- Array.zeroCreate this.Depth
+        sequenceNumbers <- Array.zeroCreate this.Depth
         let mutable bufferSize = 0
-        let stride = 12
+        let mutable bufferSizeLeaves = 0
         let mutable length = 1
 
         for i in 0 .. parts.Length - 1 do
             parts[i] <- Array.zeroCreate length
             matrices[i] <- Array.zeroCreate length
-            bufferSize <- bufferSize + length * stride
+
+            if i = parts.Length - 1 then
+                bufferSizeLeaves <- bufferSizeLeaves + length * stride
+            else
+                bufferSize <- bufferSize + length * stride
+
+            sequenceNumbers[i] <- Vector4(GD.Randf(), GD.Randf(), GD.Randf(), GD.Randf())
             length <- length * 5
 
-        matricesBuffers <- Array.zeroCreate bufferSize
+        matricesBuffer <- Array.zeroCreate bufferSize
+        matricesBufferLeaves <- Array.zeroCreate bufferSizeLeaves
         parts[0][0] <- this.CreatePart 0
 
         for li in 1 .. parts.Length - 1 do
@@ -90,29 +149,41 @@ type FractalFS() =
         multiMeshIns.Multimesh <- new MultiMesh()
         multiMeshIns.Multimesh.SetTransformFormat(MultiMesh.TransformFormatEnum.Transform3D)
         multiMeshIns.Multimesh.Mesh <- this.MeshF
-        (multiMeshIns.Multimesh.Mesh :?> SphereMesh).Material <- this.MaterialF
+        (multiMeshIns.Multimesh.Mesh :?> PrimitiveMesh).Material <- this.MaterialF
+        multiMeshIns.Multimesh.UseColors <- true
         multiMeshIns.Multimesh.InstanceCount <- bufferSize / stride
         this.AddChild multiMeshIns
+        // 叶子多网格
+        multiMeshInsLeaves <- new MultiMeshInstance3D()
+        multiMeshInsLeaves.Multimesh <- new MultiMesh()
+        multiMeshInsLeaves.Multimesh.SetTransformFormat(MultiMesh.TransformFormatEnum.Transform3D)
+        multiMeshInsLeaves.Multimesh.Mesh <- this.LeafMesh
+        (multiMeshInsLeaves.Multimesh.Mesh :?> PrimitiveMesh).Material <- this.MaterialF
+        multiMeshInsLeaves.Multimesh.UseColors <- true
+        multiMeshInsLeaves.Multimesh.InstanceCount <- bufferSizeLeaves / stride
+        this.AddChild multiMeshInsLeaves
 
         ready <- true
 
     override this._Process(delta) =
         if ready then
-            let spinAngleDelta = Mathf.DegToRad 22.5f * float32 delta
             let mutable rootPart = parts[0][0]
-            rootPart.spinAngle <- rootPart.spinAngle + spinAngleDelta
+            rootPart.spinAngle <- rootPart.spinAngle + rootPart.spinVelocity * float32 delta
             // 不然运行久了浮点数精度不够，Quaternion 不再是 Normalized，乘法报错
             if rootPart.spinAngle > Mathf.Pi * 2f then
                 rootPart.spinAngle <- rootPart.spinAngle - Mathf.Pi * 2f
             // 注意是修改 Quaternion 四元数，3D 不推荐修改 Rotation 旋转
             // Quaternion 需要 Normalized()，不然运行久了浮点数精度不够，赋值给 Transform3D 报错
             rootPart.worldRotation <-
-                (rootPart.rotation * Quaternion.FromEuler(Vector3(0f, rootPart.spinAngle, 0f)))
+                (this.Quaternion
+                 * (rootPart.rotation * Quaternion.FromEuler(Vector3(0f, rootPart.spinAngle, 0f))))
                     .Normalized()
+
+            rootPart.worldPosition <- this.Position
 
             parts[0][0] <- rootPart
             matrices[0][0] <- Transform3D(Basis(rootPart.worldRotation).Scaled Vector3.One, rootPart.worldPosition)
-            this.Transform3dToMatricesBuffers 0 <| matrices[0][0]
+            this.Transform3dToMatricesBuffer 0 0 (matrices[0][0])
             let mutable scale = 1f
             let mutable idx = 1
 
@@ -125,24 +196,37 @@ type FractalFS() =
                 for fpi in 0 .. levelParts.Length - 1 do
                     let parent = parentParts[fpi / 5]
                     let mutable part = levelParts[fpi]
-                    part.spinAngle <- part.spinAngle + spinAngleDelta
+                    part.spinAngle <- part.spinAngle + part.spinVelocity * float32 delta
                     // 不然运行久了浮点数精度不够，Quaternion 不再是 Normalized，乘法报错
                     if part.spinAngle > Mathf.Pi * 2f then
                         part.spinAngle <- part.spinAngle - Mathf.Pi * 2f
+
+                    let upAxis = (parent.worldRotation * part.rotation) * Vector3.Up
+                    let sagAxis = Vector3.Up.Cross(upAxis)
+                    let sagMagnitude = sagAxis.Length()
+
+                    let baseRotation =
+                        if sagMagnitude > 0f then
+                            let sagAxis = sagAxis / sagMagnitude
+                            let sagRotation = Quaternion(sagAxis, part.maxSagAngle * sagMagnitude)
+                            sagRotation * parent.worldRotation
+                        else
+                            parent.worldRotation
                     // 注意是修改 Quaternion 四元数，3D 不推荐修改 Rotation 旋转
                     // Quaternion 需要 Normalized()，不然运行久了浮点数精度不够，赋值给 Transform3D 报错
                     part.worldRotation <-
-                        (parent.worldRotation
+                        (baseRotation
                          * (part.rotation * Quaternion.FromEuler(Vector3(0f, part.spinAngle, 0f))))
                             .Normalized()
 
-                    part.worldPosition <- parent.worldPosition + parent.worldRotation * (1.5f * scale * part.direction)
+                    part.worldPosition <- parent.worldPosition + part.worldRotation * Vector3(0f, 1.5f * scale, 0f)
                     levelParts[fpi] <- part
 
                     levelMatrices[fpi] <-
                         Transform3D(Basis(part.worldRotation).Scaled(Vector3.One * scale), part.worldPosition)
 
-                    this.Transform3dToMatricesBuffers idx <| levelMatrices[fpi]
+                    this.Transform3dToMatricesBuffer idx li levelMatrices[fpi]
                     idx <- idx + 1
 
-            RenderingServer.MultimeshSetBuffer(multiMeshIns.Multimesh.GetRid(), matricesBuffers)
+            RenderingServer.MultimeshSetBuffer(multiMeshIns.Multimesh.GetRid(), matricesBuffer)
+            RenderingServer.MultimeshSetBuffer(multiMeshInsLeaves.Multimesh.GetRid(), matricesBufferLeaves)
--- GodotDemo/CatlikeCoding/Scenes/Basics1/Job/Fractal.cs
+++ GodotDemo/CatlikeCoding/Scenes/Basics1/Job/Fractal.cs
@@ -6,9 +6,19 @@ namespace CatlikeCodingCSharp.Scenes.Basics1.Job;
 [Tool]
 public partial class Fractal : FractalFS
 {
-    [Export(PropertyHint.Range, "1, 8")] public override int Depth { get; set; } = 4;
+    [Export(PropertyHint.Range, "3, 8")] public override int Depth { get; set; } = 4;
     [Export] public override Mesh MeshF { get; set; }
+    [Export] public override Mesh LeafMesh { get; set; }
     [Export] public override Material MaterialF { get; set; }
+    [Export] public override Gradient GradientA { get; set; }
+    [Export] public override Gradient GradientB { get; set; }
+    [Export] public override Color LeafColorA { get; set; }
+    [Export] public override Color LeafColorB { get; set; }
+    [Export(PropertyHint.Range, "0, 90")] public override float MaxSagAngleA { get; set; } = 15f;
+    [Export(PropertyHint.Range, "0, 90")] public override float MaxSagAngleB { get; set; } = 25f;
+    [Export(PropertyHint.Range, "0, 90")] public override float SpinSpeedA { get; set; } = 20f;
+    [Export(PropertyHint.Range, "0, 90")] public override float SpinSpeedB { get; set; } = 25f;
+    [Export(PropertyHint.Range, "0, 1")] public override float ReverseSpinChance { get; set; } = 0.25f;
 
     // 需要忽略 IDE 省略 partial、_Ready 等的提示，必须保留它们
     public override void _Ready() => base._Ready();
