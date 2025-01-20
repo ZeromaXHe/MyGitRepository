f4b3182ff1694d7726491770e549754fadffc8da
Zhu XH <zhuxiaohe95@163.com>
2025-01-20 23:31:44 +0800

    feature(CatlikeCoding): 参考 Pseudorandom Noise - 2 Hashing Space 教程的 1 Grid Transform ~ 3.1 Vectorized Hash

--- GodotDemo/CatlikeCoding/CatlikeCodingFS/PseudorandomNoise2/HashVisualizationFS.fs
+++ GodotDemo/CatlikeCoding/CatlikeCodingFS/PseudorandomNoise2/HashVisualizationFS.fs
@@ -42,23 +42,128 @@ type SmallXXHash =
             avalanche
     end
 
+type SmallXXHash4 =
+    struct
+        val accumulator: Vector4I
+
+        new(accumulator: Vector4I) = { accumulator = accumulator }
+        new(hash: SmallXXHash) = { accumulator = int hash.accumulator * Vector4I.One }
+
+        static member PrimeB = 0b10000101111010111100101001110111
+        static member PrimeC = 0b11000010101100101010111000111101
+        static member PrimeD = 0b00100111110101001110101100101111
+        static member PrimeE = 0b00010110010101100110011110110001
+
+        static member private RotateLeft (data: Vector4I) steps =
+            Vector4I(
+                (data.X <<< steps) ||| (data.X >>> (32 - steps)),
+                (data.Y <<< steps) ||| (data.Y >>> (32 - steps)),
+                (data.Z <<< steps) ||| (data.Z >>> (32 - steps)),
+                (data.W <<< steps) ||| (data.W >>> (32 - steps))
+            )
+
+        static member Seed(seed: Vector4I) =
+            SmallXXHash4(seed + Vector4I.One * SmallXXHash4.PrimeE)
+
+        member this.Eat(data: Vector4I) =
+            SmallXXHash4(
+                SmallXXHash4.PrimeD
+                * SmallXXHash4.RotateLeft (this.accumulator + data * SmallXXHash4.PrimeC) 17
+            )
+
+        member this.ToUint() =
+            let mutable avalanche = this.accumulator
+
+            let xorRightShift shift =
+                avalanche <-
+                    Vector4I(
+                        avalanche.X ^^^ (avalanche.X >>> shift),
+                        avalanche.Y ^^^ (avalanche.Y >>> shift),
+                        avalanche.Z ^^^ (avalanche.Z >>> shift),
+                        avalanche.W ^^^ (avalanche.W >>> shift)
+                    )
+
+            xorRightShift 15
+            avalanche <- avalanche * SmallXXHash4.PrimeB
+            xorRightShift 13
+            avalanche <- avalanche * SmallXXHash4.PrimeC
+            xorRightShift 16
+            avalanche
+    end
+
+type ShapesJob =
+    struct
+        val positions: Vector3 array
+        val normals: Vector3 array
+        val resolution: float32
+        val invResolution: float32
+        val trs: Transform3D
+
+        new(positions, normals, res, trs) =
+            { positions = positions
+              normals = normals
+              resolution = res
+              invResolution = 1f / res
+              trs = trs }
+
+        member this.Execute i =
+            let mutable uv = Vector2.Zero
+            uv.Y <- Mathf.Floor(this.invResolution * float32 i + 0.00001f)
+            uv.X <- this.invResolution * (float32 i - this.resolution * uv.Y + 0.5f) - 0.5f
+            uv.Y <- this.invResolution * (uv.Y + 0.5f) - 0.5f
+            this.positions[i] <- this.trs * Vector3(uv.X, 0f, uv.Y)
+            this.normals[i] <- (this.trs * Vector3.Up).Normalized()
+    end
+
+type HashJob =
+    struct
+        val positions: Vector3 array
+        val hashes: uint array
+        val hash: SmallXXHash
+        val domain: Transform3D
+
+        new(positions, hashes, hash, domain) =
+            { positions = positions
+              hashes = hashes
+              hash = hash
+              domain = domain }
+
+        member this.Execute i =
+            let p = this.domain * this.positions[i]
+            let u = int <| Mathf.Floor(p.X)
+            let v = int <| Mathf.Floor(p.Y)
+            let w = int <| Mathf.Floor(p.Z)
+            // 注意如果不是链式代码，则必须是 mutable；否则 Eat 虽然不报错但无效
+            let hash = this.hash.Eat(u).Eat(v).Eat(w)
+            this.hashes[i] <- hash.ToUint()
+    end
+
+type DirtyEnum =
+    | Normal = 0
+    | Resolution = 1
+    | Displacement = 2
+
 [<AbstractClass>]
 type HashVisualizationFS() =
     inherit Node3D()
 
     let mutable hashes: uint array = null
     let mutable hashesBuffer: float32 array = null
+    let mutable positions: Vector3 array = null
+    let mutable normals: Vector3 array = null
     let stride = 16 // 12 Transform + 4 Color
     let mutable config = Vector4.Zero
 
     let mutable ready = false
+    let mutable isDirty = false
     let mutable multiMeshIns: MultiMeshInstance3D = null
 
     abstract InstanceMesh: Mesh with get, set
     abstract Material: Material with get, set
     abstract Resolution: int with get, set
     abstract Seed: int with get, set
-    abstract VerticalOffset: float32 with get, set
+    abstract Displacement: float32 with get, set
+    abstract Domain: Transform3D with get, set
 
     member this.GetHashColor idx =
         let hash = hashes[idx]
@@ -66,40 +171,50 @@ type HashVisualizationFS() =
         (1f / 255f)
         * Vector3(float32 (hash &&& 255u), float32 ((hash >>> 8) &&& 255u), float32 ((hash >>> 16) &&& 255u))
 
-    member this.Execute i (hash: SmallXXHash) =
-        let mutable v = int <| Mathf.Floor(config.Y * float32 i + 0.00001f)
-        let u = i - int config.X * v - this.Resolution / 2
-        v <- v - this.Resolution / 2
-        // 注意如果不是链式代码，则必须是 mutable；否则 Eat 虽然不报错但无效
-        let hash = hash.Eat(u).Eat(v)
-        hashes[i] <- hash.ToUint()
-
-    member this.HashJob i =
-        let mutable v = Mathf.Floor(config.Y * float32 i + 0.00001f)
-        let u = float32 i - config.X * v
-
+    // 参考源码 /Assets/Materials/HashGPU.hlsl
+    member this.HashGPU i =
+        let offset = config.Z * 1f / 255f * (float32 (hashes[i] >>> 24) - 0.5f)
         hashesBuffer[i * stride] <- config.Y
         // hashesBuffer[i * stride + 1] <- 0f
         // hashesBuffer[i * stride + 2] <- 0f
-        hashesBuffer[i * stride + 3] <- config.Y * (u + 0.5f) - 0.5f // X
+        hashesBuffer[i * stride + 3] <- positions[i].X + offset * normals[i].X
         // hashesBuffer[i * stride + 4] <- 0f
         hashesBuffer[i * stride + 5] <- config.Y
         // hashesBuffer[i * stride + 6] <- 0f
-        hashesBuffer[i * stride + 7] <- config.Z * 1f / 255f * (float32 (hashes[i] >>> 24) - 0.5f)
+        hashesBuffer[i * stride + 7] <- positions[i].Y + offset * normals[i].Y
         // hashesBuffer[i * stride + 8] <- 0f
         // hashesBuffer[i * stride + 9] <- 0f
         hashesBuffer[i * stride + 10] <- config.Y
-        hashesBuffer[i * stride + 11] <- -config.Y * (v + 0.5f) + 0.5f // Godot Z 和 Unity 不同，前后相反
+        hashesBuffer[i * stride + 11] <- positions[i].Z + offset * normals[i].Z
         let color = this.GetHashColor i
         hashesBuffer[i * stride + 12] <- color.X
         hashesBuffer[i * stride + 13] <- color.Y
         hashesBuffer[i * stride + 14] <- color.Z
         hashesBuffer[i * stride + 15] <- 1f
 
+    member this.SetConfig() =
+        config <- Vector4(float32 this.Resolution, 1f / float32 this.Resolution, this.Displacement, 0f)
+
+    member this.SetDirty dirtyEnum =
+        if ready then
+            isDirty <- true
+            // 对于 C# 调用的方法，不能用 function 关键字
+            match dirtyEnum with
+            | DirtyEnum.Resolution ->
+                let length = this.Resolution * this.Resolution
+                hashes <- Array.zeroCreate length
+                positions <- Array.zeroCreate length
+                hashesBuffer <- Array.zeroCreate <| length * stride
+                multiMeshIns.Multimesh.InstanceCount <- length
+                this.SetConfig()
+            | DirtyEnum.Displacement -> this.SetConfig()
+            | _ -> ()
 
     override this._Ready() =
         let length = this.Resolution * this.Resolution
         hashes <- Array.zeroCreate length
+        positions <- Array.zeroCreate length
+        normals <- Array.zeroCreate length
         hashesBuffer <- Array.zeroCreate <| length * stride
         // 必须在代码里初始化 MultiMeshInstance3D 节点，不然场景里面既有的节点会被持久化
         // 否则 .tscn 将会包含了那些变化的 Transform 数组，一方面会很大，而且另一方面每次运行后都会被更新。其实完全没必要保存
@@ -112,19 +227,44 @@ type HashVisualizationFS() =
         multiMeshIns.Multimesh.InstanceCount <- length
         this.AddChild multiMeshIns
 
-        config <-
-            Vector4(
-                float32 this.Resolution,
-                1f / float32 this.Resolution,
-                this.VerticalOffset / float32 this.Resolution,
-                0f
-            )
+        this.SetConfig()
+        // // 监听默认变换，好像只能用这个的方式
+        // let monitor =
+        //     fun name ->
+        //         if name = "position" || name = "rotation" || name = "scale" then
+        //             isDirty <- true
+        //
+        // if Engine.IsEditorHint() then
+        //     EditorInterface.Singleton.GetInspector().add_PropertyEdited monitor
+        //
+        // // 程序集卸载前的清理逻辑（好像还是没有用……）
+        // // let handle = GCHandle.Alloc(this)
+        // let alc = AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly())
+        // GD.Print $"FS ALC: {alc} ALC is null? {alc = null}"
+        //
+        // alc.add_Unloading (fun assemblyLoadContext ->
+        //     GD.Print "Start Unloading"
+        //     // Attempt to disconnect a nonexistent connection from '@EditorInspector@5391:<EditorInspector#514775343968>'. Signal: 'property_edited', callable: 'Delegate::Invoke'.
+        //     EditorInterface.Singleton.GetInspector().remove_PropertyEdited monitor
+        //     GD.Print "End Unloading"
+        // // handle.Free()
+        // )
 
-        let hash = SmallXXHash.Seed(this.Seed)
-
-        for i in 0 .. length - 1 do
-            this.Execute i hash
-            this.HashJob i
-
-        RenderingServer.MultimeshSetBuffer(multiMeshIns.Multimesh.GetRid(), hashesBuffer)
+        isDirty <- true
         ready <- true
+
+    override this._Process(delta) =
+        if ready && isDirty then
+            isDirty <- false
+
+            let shapesJob =
+                ShapesJob(positions, normals, float32 this.Resolution, this.GlobalTransform)
+
+            let hashJob = HashJob(positions, hashes, SmallXXHash.Seed(this.Seed), this.Domain)
+
+            for i in 0 .. hashes.Length - 1 do
+                shapesJob.Execute i
+                hashJob.Execute i
+                this.HashGPU i
+
+            RenderingServer.MultimeshSetBuffer(multiMeshIns.Multimesh.GetRid(), hashesBuffer)
--- GodotDemo/CatlikeCoding/Scenes/PseudorandomNoise2/HashVisualization.cs
+++ GodotDemo/CatlikeCoding/Scenes/PseudorandomNoise2/HashVisualization.cs
@@ -6,13 +6,142 @@ namespace CatlikeCodingCSharp.Scenes.PseudorandomNoise2;
 [Tool]
 public partial class HashVisualization : HashVisualizationFS
 {
-    [Export] public override Mesh InstanceMesh { get; set; }
-    [Export] public override Material Material { get; set; }
-    [Export(PropertyHint.Range, "1, 512")] public override int Resolution { get; set; } = 16;
-    [Export] public override int Seed { get; set; }
-    [Export(PropertyHint.Range, "-2, 2")] public override float VerticalOffset { get; set; } = 1f;
+    private Mesh _instanceMesh;
+
+    [Export]
+    public override Mesh InstanceMesh
+    {
+        get => _instanceMesh;
+        set
+        {
+            _instanceMesh = value;
+            SetDirty(DirtyEnum.Normal);
+        }
+    }
+
+    private Material _material;
+
+    [Export]
+    public override Material Material
+    {
+        get => _material;
+        set
+        {
+            _material = value;
+            SetDirty(DirtyEnum.Normal);
+        }
+    }
+
+    private int _resolution = 16;
+
+    [Export(PropertyHint.Range, "1, 512")]
+    public override int Resolution
+    {
+        get => _resolution;
+        set
+        {
+            _resolution = value;
+            SetDirty(DirtyEnum.Resolution);
+        }
+    }
+
+    private int _seed;
+
+    [Export]
+    public override int Seed
+    {
+        get => _seed;
+        set
+        {
+            _seed = value;
+            SetDirty(DirtyEnum.Normal);
+        }
+    }
+
+    private float _displacement = 0.1f;
+
+    [Export(PropertyHint.Range, "-0.5, 0.5")]
+    public override float Displacement
+    {
+        get => _displacement;
+        set
+        {
+            _displacement = value;
+            SetDirty(DirtyEnum.Displacement);
+        }
+    }
+
+    private static readonly Vector3 TranslationInit = Vector3.Zero;
+    private static readonly Vector3 RotationInit = Vector3.Zero;
+    private static readonly Vector3 ScaleInit = 8 * Vector3.One;
+
+    private static Transform3D NewTransform(Vector3 pos, Vector3 rot, Vector3 scale) =>
+        new(new Basis(Quaternion.FromEuler(DegToRad(rot))).Scaled(scale), pos);
+
+    private static Vector3 DegToRad(Vector3 deg) =>
+        new(Mathf.DegToRad(deg.X), Mathf.DegToRad(deg.Y), Mathf.DegToRad(deg.Z));
+
+    private static Vector3 RadToDeg(Vector3 deg) =>
+        new(Mathf.RadToDeg(deg.X), Mathf.RadToDeg(deg.Y), Mathf.RadToDeg(deg.Z));
+
+    public override Transform3D Domain { get; set; } = NewTransform(TranslationInit, RotationInit, ScaleInit);
+
+    private Vector3 _translation = TranslationInit;
+
+    [ExportGroup("Domain")]
+    [Export]
+    public Vector3 Translation
+    {
+        get => _translation;
+        set
+        {
+            _translation = value;
+            Domain = NewTransform(_translation, _rotationD, _scaleD);
+            SetDirty(DirtyEnum.Normal);
+        }
+    }
+
+    private Vector3 _rotationD = RotationInit;
+
+    [Export]
+    public Vector3 RotationD
+    {
+        get => _rotationD;
+        set
+        {
+            _rotationD = value;
+            Domain = NewTransform(_translation, _rotationD, _scaleD);
+            SetDirty(DirtyEnum.Normal);
+        }
+    }
+
+    private Vector3 _scaleD = ScaleInit;
+
+    [Export]
+    public Vector3 ScaleD
+    {
+        get => _scaleD;
+        set
+        {
+            _scaleD = value;
+            Domain = NewTransform(_translation, _rotationD, _scaleD);
+            SetDirty(DirtyEnum.Normal);
+        }
+    }
 
     // 需要忽略 IDE 省略 partial、_Ready 等的提示，必须保留它们
-    public override void _Ready() => base._Ready();
-    // public override void _Process(double delta) => base._Process(delta);
+    public override void _Ready()
+    {
+        base._Ready();
+        // 在 F# 里面写这个逻辑会程序集卸载失败……
+        // 但监听默认变换，好像只能用这种方式。直接在 C# 种写就正常，甚至不需要上下文清理逻辑
+        if (Engine.IsEditorHint())
+            EditorInterface.Singleton.GetInspector().PropertyEdited += name =>
+            {
+                if (name is "position" or "rotation" or "scale")
+                    SetDirty(DirtyEnum.Normal);
+            };
+    }
+
+    public override void _Process(double delta) => base._Process(delta);
 }
