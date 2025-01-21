f5c8cc224a588a3867be2b40be9f005c5a6afcc2
Zhu XH <zhuxiaohe95@163.com>
2025-01-21 15:32:43 +0800

    feature(CatlikeCoding): 参考 Movement - 2 Physics

--- GodotDemo/CatlikeCoding/CatlikeCodingFS/Movement6/MovingSphereFS.fs
+++ GodotDemo/CatlikeCoding/CatlikeCodingFS/Movement6/MovingSphereFS.fs
@@ -4,43 +4,120 @@ open Godot
 
 [<AbstractClass>]
 type MovingSphereFS() =
-    inherit CsgSphere3D()
+    inherit RigidBody3D()
 
     let mutable ready = false
 
     let mutable velocity = Vector3.Zero
+    let mutable desiredVelocity = Vector3.Zero
+    let mutable desiredJump = false
+    let mutable jumpPhase = 0
+    let mutable groundContactCount = 0
+    let mutable preGroundContactCount = 0
+    let onGround () = groundContactCount > 0
+    let mutable minGroudDotProduct = 0f
+    let mutable contactNormal = Vector3.Zero
 
     abstract MaxSpeed: float32 with get, set
     abstract MaxAcceleration: float32 with get, set
-    abstract AllowedArea: Rect2 with get, set
-    abstract Bounciness: float32 with get, set
+    abstract MaxAirAcceleration: float32 with get, set
+    abstract JumpHeight: float32 with get, set
+    abstract MaxAirJumps: int with get, set
+    abstract MaxGroundAngle: float32 with get, set
 
-    override this._Ready() =
+    member this.UpdateState() =
+        velocity <- this.LinearVelocity
 
-        ready <- true
+        if onGround () then
+            jumpPhase <- 0
+
+            if groundContactCount > 1 then
+                contactNormal <- contactNormal.Normalized()
+        else
+            contactNormal <- Vector3.Up
+
+    member this.Jump() =
+        if onGround () || jumpPhase < this.MaxAirJumps then
+            jumpPhase <- jumpPhase + 1
+            let mutable jumpSpeed = Mathf.Sqrt(-2f * this.GetGravity().Y * this.JumpHeight)
+            let alignedSpeed = velocity.Dot(contactNormal)
+
+            if alignedSpeed > 0f then
+                jumpSpeed <- Mathf.Max(jumpSpeed - alignedSpeed, 0f)
+
+            velocity <- velocity + contactNormal * jumpSpeed
+
+    // member this.EvaluateCollision() =
+    //     // CharacterBody3D 用的
+    //     for i in 0 .. this.GetSlideCollisionCount() - 1 do
+    //         let collision = this.GetSlideCollision(i)
+    //         let normal = collision.GetNormal()
+    //         if normal.Y >= minGroudDotProduct then
+    //             onGround <- true
+    //             contactNormal <- normal
+
+    member this.ProjectOnContactPlane(vector: Vector3) =
+        vector - contactNormal * vector.Dot(contactNormal)
+
+    member this.AdjustVelocity delta =
+        let xAxis = (this.ProjectOnContactPlane Vector3.Right).Normalized()
+        let zAxis = (this.ProjectOnContactPlane Vector3.Back).Normalized()
+        let currentX = velocity.Dot xAxis
+        let currentZ = velocity.Dot zAxis
+
+        let acceleration =
+            if onGround () then
+                this.MaxAcceleration
+            else
+                this.MaxAirAcceleration
+
+        let maxSpeedChange = acceleration * delta
+        let newX = Mathf.MoveToward(currentX, desiredVelocity.X, maxSpeedChange)
+        let newZ = Mathf.MoveToward(currentZ, desiredVelocity.Z, maxSpeedChange)
+        velocity <- velocity + xAxis * (newX - currentX) + zAxis * (newZ - currentZ)
+
+    member this.OnValidate() =
+        minGroudDotProduct <- Mathf.Cos(Mathf.DegToRad this.MaxGroundAngle)
+
+    member this.ClearState() =
+        preGroundContactCount <- groundContactCount
+        groundContactCount <- 0
+        contactNormal <- Vector3.Zero
+
+    member this.Sphere = this.GetNode<CsgSphere3D> "Sphere"
+
+    override this._Ready() = ready <- true
 
     override this._Process(delta) =
         if ready then
             let playerInput = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down")
-            let desiredVelocity = Vector3(playerInput.X, 0f, playerInput.Y) * this.MaxSpeed
-            let maxSpeedChange = this.MaxAcceleration * float32 delta
-            velocity.X <- Mathf.MoveToward(velocity.X, desiredVelocity.X, maxSpeedChange)
-            velocity.Z <- Mathf.MoveToward(velocity.Z, desiredVelocity.Z, maxSpeedChange)
-            let displacement = velocity * float32 delta
-            let mutable newPosition = this.Position + displacement
+            desiredVelocity <- Vector3(playerInput.X, 0f, playerInput.Y) * this.MaxSpeed
+            desiredJump <- desiredJump || Input.IsActionJustPressed "Jump"
 
-            if newPosition.X < this.AllowedArea.Position.X then
-                newPosition.X <- this.AllowedArea.Position.X
-                velocity.X <- -velocity.X * this.Bounciness
-            elif newPosition.X > this.AllowedArea.End.X then
-                newPosition.X <- this.AllowedArea.End.X
-                velocity.X <- -velocity.X * this.Bounciness
+            (this.Sphere.Material :?> StandardMaterial3D).AlbedoColor <-
+                Colors.White * float32 preGroundContactCount * 0.25f
 
-            if newPosition.Z < this.AllowedArea.Position.Y then
-                newPosition.Z <- this.AllowedArea.Position.Y
-                velocity.Z <- -velocity.Z * this.Bounciness
-            elif newPosition.Z > this.AllowedArea.End.Y then
-                newPosition.Z <- this.AllowedArea.End.Y
-                velocity.Z <- -velocity.Z * this.Bounciness
+    override this._PhysicsProcess(delta) =
+        // onGround <- this.IsOnFloor() // CharacterBody3D 用的
+        // this.EvaluateCollision() // CharacterBody3D 用的
+        this.UpdateState()
+        this.AdjustVelocity <| float32 delta
+        // 重力
+        // velocity <- velocity + this.GetGravity() * float32 delta // CharacterBody3D 用的
 
-            this.Position <- newPosition
+        if desiredJump then
+            desiredJump <- false
+            this.Jump()
+
+        this.LinearVelocity <- velocity
+
+        this.ClearState()
+    // this.MoveAndSlide() |> ignore // CharacterBody3D 用的
+
+    override this._IntegrateForces(state) =
+        for i in 0 .. state.GetContactCount() - 1 do
+            let normal = state.GetContactLocalNormal(i)
+
+            if normal.Y >= minGroudDotProduct then
+                groundContactCount <- groundContactCount + 1
+                contactNormal <- contactNormal + normal
--- GodotDemo/CatlikeCoding/Scenes/Movement6/MovingSphere.cs
+++ GodotDemo/CatlikeCoding/Scenes/Movement6/MovingSphere.cs
@@ -3,15 +3,18 @@ using Godot;
 
 namespace CatlikeCodingCSharp.Scenes.Movement6;
 
-[Tool]
 public partial class MovingSphere : MovingSphereFS
 {
     [Export(PropertyHint.Range, "0, 100")] public override float MaxSpeed { get; set; } = 10f;
     [Export(PropertyHint.Range, "0, 100")] public override float MaxAcceleration { get; set; } = 10f;
-    [Export] public override Rect2 AllowedArea { get; set; } = new(-11.75f, -11.75f, 24f, 24f);
-    [Export(PropertyHint.Range, "0, 1")] public override float Bounciness { get; set; } = 0.5f;
+    [Export(PropertyHint.Range, "0, 100")] public override float MaxAirAcceleration { get; set; } = 1f;
+    [Export(PropertyHint.Range, "0, 10")] public override float JumpHeight { get; set; } = 2f;
+    [Export(PropertyHint.Range, "0, 5")] public override int MaxAirJumps { get; set; }
+    [Export(PropertyHint.Range, "0, 90")] public override float MaxGroundAngle { get; set; } = 25f;
 
     // 需要忽略 IDE 省略 partial、_Ready 等的提示，必须保留它们
     public override void _Ready() => base._Ready();
     public override void _Process(double delta) => base._Process(delta);
+    public override void _PhysicsProcess(double delta) => base._PhysicsProcess(delta);
+    public override void _IntegrateForces(PhysicsDirectBodyState3D state) => base._IntegrateForces(state);
 }
