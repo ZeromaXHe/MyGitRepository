79c871d0202ae363e6e0a62d51496c6df8ce967d
Zhu XH <zhuxiaohe95@163.com>
2025-07-30 19:46:59 +0800

    feature(GodotAutobattlerCourse): 参考《自动战斗自走棋游戏教程 第一季》 - 6 单位生成器（BV1ocqzYKECy）

---
+++ GodotDemo/GodotAutobattlerCourse/components/unit_spawner.gd
@@ -0,0 +1,38 @@
+class_name UnitSpawner
+extends Node
+
+signal unit_spawner(unit: Unit)
+
+const UNIT = preload("res://scenes/units/unit.tscn")
+
+@export var bench: PlayArea
+@export var game_area: PlayArea
+
+
+#func _ready() -> void:
+	#var robin := preload("res://data/units/robin.tres")
+	#var tween := create_tween()
+	#for i in 15:
+		#tween.tween_callback(func(): spawn_unit(robin))
+		#tween.tween_interval(0.5)
+
+
+func _get_first_available_area() -> PlayArea:
+	if not bench.unit_grid.is_grid_full():
+		return bench
+	elif not game_area.unit_grid.is_grid_full():
+		return game_area
+	return null
+
+
+func spawn_unit(unit: UnitStats) -> void:
+	var area := _get_first_available_area()
+	# TODO in the future, throw a popup error message here!
+	assert(area, "No available space to add unit to!")
+	var new_unit := UNIT.instantiate()
+	var tile := area.unit_grid.get_first_empty_tile()
+	area.unit_grid.add_child(new_unit)
+	area.unit_grid.add_unit(tile, new_unit)
+	new_unit.global_position = area.get_global_from_tile(tile) - Arena.HALF_CELL_SIZE
+	new_unit.stats = unit
+	unit_spawner.emit(new_unit)
--- GodotDemo/GodotAutobattlerCourse/scenes/arenas/arena.gd
+++ GodotDemo/GodotAutobattlerCourse/scenes/arenas/arena.gd
@@ -4,3 +4,10 @@ extends Node2D
 const CELL_SIZE := Vector2(32, 32)
 const HALF_CELL_SIZE := Vector2(16, 16)
 const QUARTER_CELL_SIZE := Vector2(8, 8)
+
+@onready var unit_mover: UnitMover = %UnitMover
+@onready var unit_spawner: UnitSpawner = %UnitSpawner
+
+
+func _ready() -> void:
+	unit_spawner.unit_spawner.connect(unit_mover.setup_unit)
